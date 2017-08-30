ba5fba584c6194198f7bd8ae49aab11969739205
tianshaoait <31428233+tianshaoait@users.noreply.github.com>
2017-08-30 09:55:35 +0800

    first
    first

---
+++ fristlibrary/src/main/java/com/tianshaoait/administrator/fristlibrary/FirstMethod.java
@@ -0,0 +1,16 @@
+package com.tianshaoait.administrator.fristlibrary;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+
+public class FirstMethod {
+
+
+    public String setFirst(String a) {
+        a = a + "gogogo";
+        return a;
+    }
+
+
+}
